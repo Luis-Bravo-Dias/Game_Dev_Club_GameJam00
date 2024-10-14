ac301c06dd29339c1a78f5166713d7bdcdec81d6
Luís DIas <luisbravodias@sapo.pt>
2024-10-14 16:47:11 +0100

    moving ducks

--- duck.js
+++ duck.js
@@ -1,4 +1,4 @@
-
+let cupWithDuck;
 
 
 window.onload = function () {
@@ -15,4 +15,27 @@ function setGame()
 		tile.id = i.toString();
 		document.getElementById("board").appendChild(tile);
 	}
+
+	setInterval(setDuck, 2000); //2000 milliseconds = 2 seconds
+}
+
+function getRandomCup()
+{
+	//generates a number between 0 and 1, then multiply by 9, with floor we round down so the results will be (0-8) integers
+	let num = Math.floor(Math.random() * 9);
+	return num.toString();
+}
+
+function setDuck()
+{
+	if (cupWithDuck)
+	{
+		cupWithDuck.innerHTML = "";
+	}
+	let duck = document.createElement("img");
+	duck.src = "./sprites/duck_1.png";
+
+	let num = getRandomCup();
+	cupWithDuck = document.getElementById(num);
+	cupWithDuck.appendChild(duck);
 }
